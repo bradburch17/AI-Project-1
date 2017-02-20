06dc9d65d34ebed132e79a183599c614e1148813
katherinemartin2017 <katherinemartin_2017@depauw.edu>
2017-02-19 21:16:47 -0500

    This is Katherine's half finished attempt at IDS.

--- src/IDS_Search.java
+++ src/IDS_Search.java
@@ -4,62 +4,97 @@ import java.util.Stack;
 public class IDS_Search {
 	
 	private int moveCounter = 0;
-	private Stack<int[][]> stateStack = new Stack<int[][]>();
-	private int[][] startState = new int[3][3];
-	private int[][] currentState = new int[3][3];
-	private State state = new State();
-	private int[][] goalState = new int[3][3];
-	private ArrayList<int[][]> closed = new ArrayList<int[][]>();
+	private Stack<Node> stateStack = new Stack<>();
+	private Node startNode;
+	private State currentState;
+	private Node currentNode;
+	private Node goalNode;
+	private boolean isGoalFound;
+	private ArrayList<Node> closed = new ArrayList<Node>();
 	
-	public IDS_Search(int[][] start)
+	public IDS_Search(Node goalNode)
 	{
-		this.goalState = state.getGoalState();
-		this.startState = start;
+		this.goalNode = goalNode;
 	}
 	
-	public void succesor(int[][] currentState)
-	{
-		int[][] newState = new int[3][3];
-		State state = new State();
+	public void runIDS(Node startNode){
 		
-		newState = state.moveDown(currentState);
+		int depth = 0;
 		
-		if (newState != null)
-		{
-			stateStack.push(newState);
-		}
-		
-		newState = state.moveUp(currentState);
-		
-		if (newState != null)
-		{
-			stateStack.push(newState);
-		}
-		
-		newState = state.moveLeft(currentState);
-		
-		if (newState != null)
-		{
-			stateStack.push(newState);
-		}
-		
-		newState = state.moveRight(currentState);
-		
-		if (newState != null)
-		{
-			stateStack.push(newState);
+		while(!isGoalFound){
+			System.out.println();
+			dfs(startNode, depth);
+			depth++;
 		}
 	}
 	
-	public void search(int depthLimit)
-	{
-		boolean found = false;
-		boolean exist = false;
-		boolean checkElement = true;
-		int[][] temp = new int[3][3];
+	public void dfs(Node startNode, int depth){
+		Stack<int[][]> stack = new Stack<int[][]>();
+		currentState.setState(startNode.getCurrentState());
+		currentState.setDepthLimit(0);
+		stateStack.push(startNode);
 		
-		currentState = stateStack.pop();
-		closed.add(currentState);
-		succesor(currentState);
+		while( !stack.isEmpty() ){
+			
+			Node actualNode = stateStack.pop();
+			System.out.println(actualNode+"");
+			
+			if(actualNode.getCurrentState() == currentState.getGoalState()){
+				// GOAL NODE HAS BEEN FOUND
+				isGoalFound = true;
+				return;
+				
+			}
+			else if(actualNode.getCurrentState() != currentState.getGoalState()){
+				currentState.setDepthLimit(currentState.getDepthLimit()+1);
+			}
+		}
+			
 	}
+	
+//	public void succesor(int[][] currentState)
+//	{
+//		int[][] newState = new int[3][3];
+//		State state = new State();
+//		
+//		newState = state.moveDown(currentState);
+//		
+//		if (newState != null)
+//		{
+//			stateStack.push(newState);
+//		}
+//		
+//		newState = state.moveUp(currentState);
+//		
+//		if (newState != null)
+//		{
+//			stateStack.push(newState);
+//		}
+//		
+//		newState = state.moveLeft(currentState);
+//		
+//		if (newState != null)
+//		{
+//			stateStack.push(newState);
+//		}
+//		
+//		newState = state.moveRight(currentState);
+//		
+//		if (newState != null)
+//		{
+//			stateStack.push(newState);
+//		}
+//	}
+//	
+//	public void search(int depthLimit)
+//	{
+//		boolean found = false;
+//		boolean exist = false;
+//		boolean checkElement = true;
+//		int[][] temp = new int[3][3];
+//		
+//		currentState = stateStack.pop();
+//		closed.add(currentState);
+//		succesor(currentState);
+//	}
 }
